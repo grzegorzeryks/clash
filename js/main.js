870dd9402e05ffd1567898b2aee3d78fedbdbd01
grzegorzeryks <grzegorzeryks@gmail.com>
2018-04-18 22:53:55 +0200

    animation corrected

--- js/main.js
+++ js/main.js
@@ -2,21 +2,29 @@ var head = document.querySelector('.warcze');
 var snd = new Audio('evilsound.mp3');
 var slashSound = new Audio('slash.wav');
 
-// head.addEventListener('click', function(){
-//     snd.play();
-//     head.classList.add('run');
-//      setTimeout(function(){head.classList.remove('run')}, 1000);
-//   });
+head.addEventListener('click', function() {
+
+  setTimeout(function() {
+    head.classList.remove('run')
+  }, 1000);
+});
 
 //slash parent
 var slash = document.querySelector('.slash');
+var state = true;
 
 slash.addEventListener('click', (function() {
   slashSound.play();
   slash.classList.add('slashRun');
-  snd.play();
+
   setTimeout(function() {
     slash.classList.remove('slashRun');
+    snd.play();
+    head.classList.add('run');
+    setTimeout(() => {
+      head.classList.remove('run');
+    }, 3000)
+
   }, 2000);
 }));
 
