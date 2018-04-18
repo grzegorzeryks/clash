f801a72372f48241ea8024d5790d761db1692efb
grzegorzeryks <grzegorzeryks@gmail.com>
2018-04-18 17:40:12 +0200

    animated character

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-console.log('elo działam');
-
 var head = document.querySelector('.warcze');
-var snd = new Audio("evilsound.mp3");
-var animState = head.style.animationPlayState;
-console.log(animState);
+var snd = new Audio('evilsound.mp3');
+var slashSound = new Audio('slash.wav');
 
-head.addEventListener('click', function(){
-    snd.play();
-    head.classList.add('run');
-     setTimeout(function(){head.classList.remove('run')}, 1000);
-  });
+// head.addEventListener('click', function(){
+//     snd.play();
+//     head.classList.add('run');
+//      setTimeout(function(){head.classList.remove('run')}, 1000);
+//   });
+
+//slash parent
+var slash = document.querySelector('.slash');
+
+slash.addEventListener('click', (function() {
+  slashSound.play();
+  slash.classList.add('slashRun');
+  snd.play();
+  setTimeout(function() {
+    slash.classList.remove('slashRun');
+  }, 2000);
+}));
+
+// document.getElementById("p2").style.color = "blue";
