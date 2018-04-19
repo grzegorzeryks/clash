83b8929d5990c6ba9391b88a09fe6a5b2286efab
grzegorzeryks <grzegorzeryks@gmail.com>
2018-04-19 17:11:14 +0200

    more

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
-var head = document.querySelector('.warcze');
-var snd = new Audio('evilsound.mp3');
-var slashSound = new Audio('slash.wav');
-
-head.addEventListener('click', function() {
-
-  setTimeout(function() {
-    head.classList.remove('run')
-  }, 1000);
-});
+let head = document.querySelector('.warcze');
+let evil = new Audio('evilsound.mp3');
+let slashSound = new Audio('slash.wav');
+let monsta = document.querySelector('.monsta');
+let slash = document.querySelector('.slash');
+let healthLeft = document.querySelector('.health-left');
+let healthCountRight = 100;
+let win = document.querySelector('.win');
 
 //slash parent
-var slash = document.querySelector('.slash');
-var state = true;
+let atak = document.querySelector('.atak');
 
-slash.addEventListener('click', (function() {
+atak.addEventListener('click', (function() {
   slashSound.play();
   slash.classList.add('slashRun');
+  monsta.classList.add('monstaRun');
 
   setTimeout(function() {
     slash.classList.remove('slashRun');
-    snd.play();
-    head.classList.add('run');
-    setTimeout(() => {
-      head.classList.remove('run');
-    }, 3000)
+    monsta.classList.remove('monstaRun');
 
-  }, 2000);
+    let hRchange = healthCountRight + '%';
+
+    healthLeft.style.width = hRchange;
+  }, 1400);
+  healthCountRight = healthCountRight - 25;
+
+  if (healthCountRight > 0) {
+    return healthCountRight;
+  } else {
+    setTimeout(() => {
+      win.style.display = 'block';
+      win.style.animationPlayState = 'running';
+      evil.play();
+      healthCountRight = 100;
+      console.log(healthCountRight);
+      let restart = healthCountRight + '%';
+
+      healthLeft.style.width = restart;
+      //zmień to na podmianke klasy potem
+      //settimeouta do zmiany nunning na paused
+      setTimeout(() => {
+        win.style.display = 'none';
+
+      }, 2000)
+    }, 2000);
+
+  }
 }));
 
 // document.getElementById("p2").style.color = "blue";
